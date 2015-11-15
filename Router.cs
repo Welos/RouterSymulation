a7cb74566f3bc1670ba71d1ac82cbf3a0b366f23
bbielecki <bartlomiejbielecki1@gmail.com>
2015-11-15 16:33:58 +0100

    Update Router.cs

--- Router.cs
+++ Router.cs
@@ -2,22 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Router
 {
     class Program
     {
+        static void TestOfUnsortedList(int A, int B, int M, int N)
+        {
+            UnsortedList<int> l1 = new UnsortedList<int>();
+            l1.Test( A, B, M, N);
+
+        }
+
+        static void TestOfHeap(int A, int B, int M, int N)
+        {
+            Heap<int> heap = new Heap<int>(A + 1);
+            heap.Test(A, B, M, N);
+
+        }
+
         static void Main(string[] args)
         {
+            int A = 10000; int B = 10001; int M = 20; int N = 15;
+            Stopwatch stopWatchAll = new Stopwatch();
 
-            Test t1 = new Test(100000,100000,15,10);
-            Console.WriteLine();
-            t1.HeapTest();
-            Console.WriteLine();
-            t1.UnsortedListTest();
-            Console.WriteLine();
-
+            stopWatchAll.Start();
+            Console.WriteLine("List");
+            TestOfUnsortedList(A, B, M, N);
+            Console.WriteLine("Heap");
+            TestOfHeap(A, B, M, N);
+            stopWatchAll.Stop();
+            Console.WriteLine("Total operation time: {0}", stopWatchAll.Elapsed);
         }
     }
 }
