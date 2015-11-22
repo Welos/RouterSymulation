64c59c304917cadf9497e997065e281df15c08df
bbielecki <bartlomiejbielecki1@gmail.com>
2015-11-22 12:42:21 +0100

    Update Object.cs

--- Object.cs
+++ Object.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace Router
 {
-    public class Object<T> where T: new()
-    {
-        static int number;
+    public class Object<T> where T : new()  // szablonowa klasa objektu obslugiwanego przez kolejki priorytetowe / 
+    {                                       // najwazniejsza zmienna Objektu jest key, sluzy ona do segregacji elementow/ zmienna statyczna pozwala na zlizcanie Objektow
+        static int number;                  // pozostale zmienne nie sa wykorzystywane podczas testow, sluza jedynie pokazaniu jak moze wygladac pakiet w symulacji systemu
         public int key;
         public T ob
         {
             private set;
             get;
         }
-        private double serviceTime;
+        private double serviceTime;   
         private int importance;
 
-        public Object ()
+        public Object()
         {
             this.key = 0;
             this.serviceTime = 0;
@@ -26,9 +26,9 @@ namespace Router
             this.ob = new T();
             number++;
         }
-        public Object( int k, double sT, int i, T obj)
+        public Object(int k, double sT, int i, T obj)
         {
-            Random rand= new Random();
+            Random rand = new Random();
             this.key = k;
             this.serviceTime = sT;
             this.importance = i;
